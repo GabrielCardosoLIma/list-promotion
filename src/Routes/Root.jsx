ebdb2ce9823874db644a83a0178181067d4c6d1c
GABRIEL CARDOSO LIMA <gabriel.clima15@senacsp.edu.br>
2022-04-11 21:59:15 -0300

    att1104

---
+++ src/Routes/Root.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { BrowserRouter as Router, 
+    Routes, 
+    Route, 
+    Link 
+  } from "react-router-dom";
+
+import PagesPromotionForm from "../pages/Promotion/Form/Form";
+import PagesPromotionList from "../pages/Promotion/List/List";
+
+const Root = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<PagesPromotionList />} />
+        <Route path="/create" element={<PagesPromotionForm/>} />
+        <Route path="/edit/:id" element={<PagesPromotionForm/>} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default Root;
