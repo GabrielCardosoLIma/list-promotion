5c466fc03f6e792c8397e891933db4f19bfb8e4d
GABRIEL CARDOSO LIMA <gabriel.clima15@senacsp.edu.br>
2022-04-27 21:02:53 -0300

    add_metodo_search

--- src/components/promotion/Search/Search.jsx
+++ src/components/promotion/Search/Search.jsx
@@ -11,25 +11,30 @@ const PromotionSearch = () => {
     const [search, setSearch] = useState('');
 
     useEffect( () => {
-  axios.get("http://localhost:5000/promotions?_embed=comments")
+    const params = {};
+      if (search) {
+        params.title_like = search;
+      }
+    axios.get("http://localhost:5000/promotions?_embed=comments", {params})
     .then((response) => {
       // console.log(response.data);
       setPromotions(response.data);
      }
     );
 
-}, [] );
+}, [search] );
 
     return ( 
         <>
-        <header>
-          <h1>Promo Show</h1>
+        <header className={SearchCSS.PromotionSearchHeader}>
+          <h1>Promo Show!</h1>
           <Link to="/create">Nova Promoção</Link>
         </header>
         <input type="search" 
         className={SearchCSS.PromotionSearchInput}
         placeholder="Buscar"
         value={search}
+        onChange={(ev) => setSearch(ev.target.value)}
         />
         {promotions.map((promotions) =>(
           <PromotionCard promotion={promotions} key={promotions.id}/>
